c6ffe555fb19af3f429e14f5a27ae92b26be2cf9
RafaelCoFernandes <rafaelfernandesxcvii@gmail.com>
2021-05-08 17:50:01 -0300

    inserir a URL do linkedin no projeto

--- frontend/src/components/Footer/index.tsx
+++ frontend/src/components/Footer/index.tsx
@@ -2,7 +2,7 @@ const Footer = () => {
     return (
         <footer className="footer mt-auto py-3 bg-dark">
             <div className="container">
-                <p className="text-light">App desenvolvido por <a href="https://github.com/RafaelCoFernandes" target="_blank" rel="noreferrer">Rafael Fernandes</a></p>
+                <p className="text-light">App desenvolvido por <a href="https://linkedin.com/in/rafael-fernandes-32304b187" target="_blank" rel="noreferrer">Rafael Fernandes</a></p>
                 <p className="text-light"><small><strong>Semana Spring React</strong><br />
             Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
             </div>
